795a49149660a54390ccb4ff61076dbca410956e
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-03-27 14:10:43 +0800

    chore: added method create post at post controller

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -4,7 +4,33 @@ const Post = db.posts;
 const Op = db.Sequelize.Op;
 
 // create
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Title can not be empty",
+    });
+    return;
+  }
+
+  // create a Post
+  const post = {
+    title: req.body.title,
+    content: req.body.content,
+    published: req.body.published || false,
+  };
+
+  // save Post in the database
+  Post.create(post)
+    .then((data) => {
+      res.send(data);
+    })
+    .cath((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Post.",
+      });
+    });
+};
 
 // retrieve all
 exports.findAll = (req, res) => {};
